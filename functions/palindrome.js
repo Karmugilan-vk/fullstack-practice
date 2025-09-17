21e2b51f58244f33d0792f0de0cfa8db0cc6d772
Karmugilan R <karmugilanrama@gmail.com>
2025-09-17 09:21:21 +0530

    Remove empty functions folder

--- functions/palindrome.js
+++
@@ -1,7 +0,0 @@
-function isPalindrome(str){
-    const reversed = str.split("").reverse().join("");
-    return str === reversed;
-}
-
-console.log(isPalindrome("markram"));
-console.log(isPalindrome("javascript"));
