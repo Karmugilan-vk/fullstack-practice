defa6f4577e0fd8f6100b862dfac8b20caea64ab
Karmugilan R <karmugilanrama@gmail.com>
2025-09-19 09:48:23 +0530

    Arrow Function Practice

---
+++ ArrowFunction/evenNumbers.js
@@ -0,0 +1,3 @@
+const num = [18,7,45,8,3,17,33,20,10];
+const even = num.filter(n => n % 2 === 0);
+console.log(even);
---
+++ ArrowFunction/stringLength.js
@@ -0,0 +1,3 @@
+const words = ["Madhu","Dharshini","Ragavi","Megalai","Nandhini"];
+const lengths = words.map(word => word.length);
+console.log(lengths);
