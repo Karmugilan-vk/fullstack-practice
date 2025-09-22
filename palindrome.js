7a208b143b538e4e427e403772abf4eb0a4bd92a
Karmugilan R <karmugilanrama@gmail.com>
2025-09-22 22:26:49 +0530

    Delete palindrome.js

--- palindrome.js
+++
@@ -1,6 +0,0 @@
-function isPalindrome(str){
-    const reversed = str.split("").reverse().join("");
-    return str === reversed;
-}
-console.log(isPalindrome("markram"));
-console.log(isPalindrome("palindrom"));
