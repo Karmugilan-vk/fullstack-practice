2b203b6f4781e20d2c5ebcfebef04339cbea2b66
Karmugilan R <karmugilanrama@gmail.com>
2025-09-26 20:14:50 +0530

    JS Practice

--- capitalizeWords.js
+++
@@ -1,5 +0,0 @@
-function captilizeWords(sentence){
-    return sentence.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
-}
-
-console.log(captilizeWords("presidio trainig in full stack"));
