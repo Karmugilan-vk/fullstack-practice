fbcdf67c8a884e11101401614dd3f5374b64961f
Karmugilan R <karmugilanrama@gmail.com>
2025-09-26 20:14:27 +0530

    Delete longestWord.js

--- longestWord.js
+++
@@ -1,5 +0,0 @@
-function longestWord(sentence){
-    return sentence.split(" ").reduce((longest,word)=>word.length > longest.length ? word:longest,"");
-}
-
-console.log(longestWord("I am learning Javascript basics"));
